4b4c748aacee3a8435e7bd16cf8bbabbd1e77682
Lavínia Zactiti <laviniafranqueiro@gmail.com>
2023-08-09 16:14:03 -0300

    adiciona a frase

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
 import pandas as pd
 
-df = pd.read_ukraine.csv("https://raw.githubusercontent.com/mateuspestana/datasets_aulas/main/ukraine.csv")
+df = pd.read_csv("https://raw.githubusercontent.com/mateuspestana/datasets_aulas/main/ukraine.csv")
+print("Abre a base de dados")
+print(df)
